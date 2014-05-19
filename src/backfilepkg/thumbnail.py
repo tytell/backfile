cc21011b4dfce2ccee07941a3349dd4e5e94aded
Eric Tytell <eric.tytell@tufts.edu>
2014-05-19 10:30:37 -0400

    Image and video thumbnails seem to be working using OpenCV

--- src/backfilepkg/thumbnail.py
+++ src/backfilepkg/thumbnail.py
@@ -12,7 +12,7 @@ import cv2
 import numpy as np
 
 READLENGTH = 500
-IMAGESIZE = 256
+THUMBSIZE = 256
 
 class Thumbnail_Image(object):
     '''
@@ -28,10 +28,14 @@ class Thumbnail_Image(object):
             self.path = path
 
         imfull = cv2.imread(self.path)
-        r = IMAGESIZE / max(imfull)
-        dim = tuple([int(a*r) for a in imfull.shape])
-        
-        self.im = cv2.resize(imfull,dim, interpolation = cv2.INTER_NEAREST)
+        self.size = imfull.shape[:2]
+        if (imfull.shape[0] > THUMBSIZE) or (imfull.shape[1] > THUMBSIZE):
+            rat = float(THUMBSIZE) / max(imfull.shape[:2])
+            dim = (int(imfull.shape[0]*rat), int(imfull.shape[1]*rat))
+            
+            self.im = cv2.resize(imfull,dim, interpolation = cv2.INTER_NEAREST)
+        else:
+            self.im = imfull
         
     def from_hdf5(self, h5parent=None):
         if h5parent:
@@ -46,29 +50,115 @@ class Thumbnail_Image(object):
         if h5parent:
             self.h5parent = h5parent
 
-        data = cv2.imencode('.jpg',self.im, [cv2.cv.IMWRITE_JPEG_QUALITY, 95])
+        success,data = cv2.imencode('.jpg',self.im, [cv2.IMWRITE_JPEG_QUALITY, 95])
+        
+        if success:
+            if 'Thumbnail' in h5parent:
+                h5obj = h5parent['Thumbnail']
+                h5obj.resize(data.shape)
+                h5obj[:] = data
+            else:
+                h5obj = h5parent.create_dataset('Thumbnail',data=data, 
+                                                maxshape=(None,1))
+            h5obj.attrs['Size'] = self.size
+            if len(self.im.shape) == 3:
+                h5obj.attrs['Depth'] = self.im.shape[2]
+            else:
+                h5obj.attrs['Depth'] = 1
+            h5obj.attrs['Type'] = str(self.im.dtype)
+        
+
+class Thumbnail_Video(object):
+    '''
+    Class to read thumbnails from video type files
+    '''
+    
+    def __init__(self, path=None, h5parent=None):
+        self.path = path
+        self.h5parent = h5parent
+        
+    def from_file(self, path=None):
+        if path:
+            self.path = path
+
+        cap = cv2.VideoCapture(self.path)
+        
+        success, im1 = cap.read()
+        if not success:
+            self.im = None
+            return
+            
+        cap.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO,0.5)
+        success, im2 = cap.read()
+        if not success:
+            self.im = None
+            return
+        
+        cap.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO,0.99)
+        success, im3 = cap.read()
+        if not success:
+            self.im = None
+            return
+
+        self.size = (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
+                     int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+                     
+        self.nframes = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+        self.fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
+        
+        cap.release()
+        
+        if (im1.shape[0] > THUMBSIZE) or (im1.shape[1] > THUMBSIZE):
+            rat = float(THUMBSIZE) / max(im1.shape[:2])
+            dim = (int(im1.shape[0]*rat), int(im1.shape[1]*rat))
+            
+            self.im = [cv2.resize(im1,dim, interpolation = cv2.INTER_NEAREST),
+                       cv2.resize(im2,dim, interpolation = cv2.INTER_NEAREST),
+                       cv2.resize(im3,dim, interpolation = cv2.INTER_NEAREST)]
+        else:
+            self.im = [im1, im2, im3]
+        
+    def from_hdf5(self, h5parent=None):
+        if h5parent:
+            self.h5parent = h5parent
         
         if 'Thumbnail' in h5parent:
             h5obj = h5parent['Thumbnail']
-            h5obj.resize(self.im.shape)
-        else:
-            h5obj = h5parent.create_dataset('Thumbnail',shape=self.im.shape, 
-                                            maxshape=(None,),
-                                            dtype=np.uint8)
-        h5obj[:] = data
-         
+            data = h5obj[:]
 
-#==============================================================================
-#class Thumbnail_Video(object):
-# vidcap.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO,0.9)
-# Out[14]: True
-# 
-# In [15]: vid^Cp.set(cv2.cv.CV_CAP_PROP_POS_AVI_RATIO,0.9)
-# KeyboardInterrupt
-# 
-# In [15]: success, image = vidcap.read()
-#==============================================================================
-        
+            self.im = [cv2.imdecode(data[:,i],1) for i in xrange(data.shape[1])]
+            self.size = h5obj.attrs['Size']
+            self.nframes = h5obj.attrs['NFrames']
+            self.fps = h5obj.attrs['FramesPerSec']
+            
+            
+    def to_hdf5(self, h5parent=None):
+        if h5parent:
+            self.h5parent = h5parent
+
+        ok, data = zip(*[cv2.imencode('.jpg',im1, [cv2.IMWRITE_JPEG_QUALITY, 95]) for im1 in self.im])
+        if all(ok):
+            vlendatatype = h5py.special_dtype(vlen=data[0].dtype)
+            try:
+                h5obj = h5parent.require_dataset('Thumbnail',dtype=vlendatatype,
+                                                 shape=(3,))
+            except TypeError:
+                del h5parent['Thumbnail']
+                h5obj = h5parent.require_dataset('Thumbnail',dtype=vlendatatype,
+                                                 shape=(3,))
+                
+            for (i,data1) in enumerate(data):
+                h5obj[i] = data1[:,0]
+
+            h5obj.attrs['Size'] = self.size
+            if len(self.im[0].shape) == 3:
+                h5obj.attrs['Depth'] = self.im[0].shape[2]
+            else:
+                h5obj.attrs['Depth'] = 1
+            h5obj.attrs['Type'] = str(self.im[0].dtype)
+            h5obj.attrs['NFrames'] = self.nframes
+            h5obj.attrs['FramesPerSec'] = self.fps
+            
 class Thumbnail_Text(object):
     '''
     Class to read thumbnails from text type files
@@ -135,7 +225,8 @@ class Thumbnail_Text(object):
                 
             
 thumbtypes = {'image': Thumbnail_Image,
-                 'text': Thumbnail_Text}
+                 'text': Thumbnail_Text,
+                 'video': Thumbnail_Video}
 
 def get_thumbnail(path=None, h5obj=None):
     thumb = None
